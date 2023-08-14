2cf1be5ef1580e2b4fc2859bb0629ec07729bc24
Nim <nimlot.elise@gmail.com>
2023-08-14 12:09:32 +0200

    Rangement

--- Assets/Scripts/MovementCamera.cs
+++ Assets/Scripts/MovementCamera.cs
@@ -6,14 +6,18 @@ using DG.Tweening;
 public class MovementCamera : MonoBehaviour
 {
     [SerializeField] Transform Camera;
+    [SerializeField] float X;
+    [SerializeField] float Y;
+    [SerializeField] float Xtime;
+    [SerializeField] float Ytime;
 
     void Start()
     {
-        transform.DOMoveX(2f, 16f)
+        transform.DOMoveX(X, Xtime)
              .SetLoops(-1, LoopType.Yoyo)
              .SetEase(Ease.InOutSine);
 
-        transform.DOMoveY(12.1f, 8f)
+        transform.DOMoveY(Y, Ytime)
             .SetLoops(-1, LoopType.Yoyo)
             .SetEase(Ease.InOutSine);
 
