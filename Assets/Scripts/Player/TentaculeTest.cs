081564b8046b15bf5b0984030bdc0cc19e9c1633
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2023-09-05 18:32:18 +0200

    Tentacules

---
+++ Assets/Scripts/Player/TentaculeTest.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentaculeTest : MonoBehaviour
+{
+    public List<Transform> segmentsTransform;
+
+    public Transform targetDir;
+    public float targetDist;
+    public float smoothSpeed;
+    
+    private Vector3[] segmentV;
+    private void Start()
+    {
+        Init();
+        segmentV = new Vector3[transform.childCount];
+    }
+
+    void Init()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            segmentsTransform.Add(transform.GetChild(i));
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = segmentsTransform.Count - 1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                segmentsTransform[i].position = transform.position;
+                segmentsTransform[i].rotation = targetDir.rotation;
+            }
+            else
+            {
+                segmentsTransform[i].position = Vector3.SmoothDamp(segmentsTransform[i].position, segmentsTransform[i - 1].position + segmentsTransform[0].right * targetDist,
+                    ref segmentV[i], smoothSpeed/(segmentsTransform.Count-i));
+            }
+        }
+    }
+}
